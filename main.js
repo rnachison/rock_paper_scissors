43cb37b7aa140ae509a007d47a54c491ffcc08d3
Rebecca Nachison <rnachison@Rebeccas-MacBook-Air.local>
2014-10-03 12:10:33 -0700

    Add function to append computer's 'I chose' text

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ alert("Let's fight!");
 var choose = function() { 
   var userWeapon = document.getElementById('userChoice').value;
 };
-var compChoice = Math.floor(Math.random() * 6) + 1;
-if (compChoice <= 2) {
+var compChoice = Math.floor(Math.random() * 3);
+if (compChoice = 1) {
   compWeapon = "rock";
-} else if (compChoice <= 4) {
+} else if (compChoice = 2) {
   compWeapon = "paper";
 } else {
   compWeapon = "scissors";
 };
 console.log(compWeapon);
+
+var choseText = function() {
+var compWeaponNode=document.createTextNode(compWeapon);
+node.appendChild(compWeaponNode);
+document.getElementById("chose").appendChild(node);
+}
